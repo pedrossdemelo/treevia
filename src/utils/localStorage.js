579db3aa5864020f6d9c4087d497733d8dc3d15f
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 03:03:34 -0300

    feat(utils): add localStorage util fn

---
+++ src/utils/localStorage.js
@@ -0,0 +1,18 @@
+export function loadState(key) {
+  try {
+    const serializedState = localStorage.getItem(key);
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+}
+
+export function saveState(key, state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem(key, serializedState);
+  } catch (err) {
+    console.error(err);
+  }
+}
