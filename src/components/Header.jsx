4ee5b865d7b7b04a9b46916c930ec9590f028cd9
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 20:10:05 -0300

    fix: clamp overflow name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,18 @@ import md5 from "crypto-js/md5";
 export function Header() {
   const name = useSelector(state => state.player.name);
   const gravatarEmail = useSelector(state => state.player.gravatarEmail);
-  const score = useSelector(state => state.player.score);
+  const highScore = useSelector(state => state.player.highScore);
   const hashedImageUrl = md5(gravatarEmail).toString();
 
   return (
-    <header className="bg-bgcolor2 m-4 py-2 px-4 h-14 rounded-xl flex items-center justify-between overflow-hidden">
-      <h1 className="text-xl text-center">Score: {score}</h1>
-      <div className="flex justify-end gap-4 items-center">
-        <h1 className="text-xl">{name}</h1>
+    <header className="bg-bgcolor2 m-4 py-2 gap-2 px-4 h-14 rounded-xl flex items-center justify-between overflow-hidden">
+      <h1 className="text-xl text-center shrink-0">High score: {highScore}</h1>
+      <div className="flex justify-end gap-4 grow items-center">
+        <h1 className="text-xl shrink line-clamp-1 overflow-ellipsis">{name}</h1>
         <img
           src={`https://www.gravatar.com/avatar/${hashedImageUrl}`}
           alt={name}
-          className="rounded-full h-10"
+          className="rounded-full h-10 shrink-0"
         />
       </div>
     </header>
