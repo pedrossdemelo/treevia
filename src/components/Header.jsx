900073101c8ebb059121b07906d295ea012c29e6
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 04:09:06 -0300

    feat: style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import md5 from 'crypto-js/md5';
+import React from "react";
+import { useSelector } from "react-redux";
+import md5 from "crypto-js/md5";
 
 export function Header() {
-  const name = useSelector((state) => state.player.name);
-  const gravatarEmail = useSelector((state) => state.player.gravatarEmail);
-  const score = useSelector((state) => state.player.score);
-  const hashGerada = md5(gravatarEmail).toString();
+  const name = useSelector(state => state.player.name);
+  const gravatarEmail = useSelector(state => state.player.gravatarEmail);
+  const score = useSelector(state => state.player.score);
+  const hashedImageUrl = md5(gravatarEmail).toString();
 
   return (
-    <header>
-      <img
-        src={ `https://www.gravatar.com/avatar/${hashGerada}` }
-        alt={ name }
-      />
-      <span>{name}</span>
-      <span>{score}</span>
+    <header className="bg-bgcolor2 m-4 py-2 px-4 h-14 rounded-xl flex items-center justify-between overflow-hidden">
+      <h1 className="text-xl text-center">Score: {score}</h1>
+      <div className="flex justify-end gap-4 items-center">
+        <h1 className="text-xl">{name}</h1>
+        <img
+          src={`https://www.gravatar.com/avatar/${hashedImageUrl}`}
+          alt={name}
+          className="rounded-full h-10"
+        />
+      </div>
     </header>
   );
 }
