2accead0a57b7ab6ff548120142bacb1f120a678
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 03:02:59 -0300

    feat: scaffold pages and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
-function App() {
-  return <div>Treevia</div>;
-}
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Login, Game, Settings, Feedback } from "./pages";
+import { Header } from "./components";
+import renderWithHeader from "./utils/renderWithHeader";
+import { useLocation } from "react-router-dom";
 
-export default App;
+export default function App() {
+  const { pathname } = useLocation();
+
+  return (
+    <>
+      {renderWithHeader(pathname) && <Header />}
+
+      <main className="max-w-xl mx-auto grow">
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/feedback">
+            <Feedback />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
+}
