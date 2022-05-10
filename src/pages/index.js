073ccb691f63ef125147f1c44d53815debae01cb
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 03:05:42 -0300

    chore: add index to pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Feedback } from "./Feedback";
+export { default as Game } from "./Game";
+export { default as Login } from "./Login";
+export { default as Settings } from "./Settings";
