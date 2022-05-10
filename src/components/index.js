f97c32a90403120c2eedab83c022c7aabe7f3cfd
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 03:05:00 -0300

    chore: add index to components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as AnswerButton } from "./AnswerButton";
+export { default as Header } from "./Header";
+export { default as Question } from "./Question";
+export { default as Timer } from "./Timer";
