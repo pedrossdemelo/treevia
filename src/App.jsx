5654cd424e0c6ef677d509011bc4248e3459ead1
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-11 02:12:34 -0300

    feat: add ThemeButton to toggle theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Login, Game, Settings, Feedback } from "./pages";
 import { Header } from "./components";
@@ -6,6 +6,8 @@ import renderWithHeader from "./utils/renderWithHeader";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { toggleTheme } from "./utils";
+import * as Icons from "phosphor-react";
 
 export default function App() {
   const { pathname } = useLocation();
@@ -16,7 +18,7 @@ export default function App() {
     <>
       {renderWithHeader(pathname) && <Header />}
 
-      <main className="max-w-2xl mb-4 mx-auto grow">
+      <main className="max-w-2xl pb-20 mx-auto grow">
         <Switch>
           <Route exact path="/">
             {loggedIn ? <Redirect to="/game" /> : <Login />}
@@ -32,6 +34,33 @@ export default function App() {
           </Route>
         </Switch>
       </main>
+
+      <ThemeButton />
     </>
+  );
+}
+
+function ThemeButton() {
+  const isDark = document.documentElement.classList.contains("dark");
+
+  const [currTheme, setCurrTheme] = useState(isDark ? "dark" : "light");
+
+  const [icon, setIcon] = useState(isDark ? "MoonStars" : "SunDim");
+
+  useEffect(() => {
+    setIcon(currTheme === "dark" ? "MoonStars" : "SunDim");
+  }, [currTheme]);
+
+  const Icon = Icons[icon];
+
+  return (
+    <button
+      className="fixed bottom-4 right-4 bg-bgcolor p-2 rounded-full flex items-center justify-center"
+      onClick={() =>
+        toggleTheme() || setCurrTheme(currTheme === "dark" ? "light" : "dark")
+      }
+    >
+      <Icon size="24" weight="bold" />
+    </button>
   );
 }
