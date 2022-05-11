fdb3f755511260ed6fb24449bf32a1e97cfa29c2
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 22:59:24 -0300

    fix: select component styling

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,4 +1,4 @@
-import { ArrowRight, SignOut } from "phosphor-react";
+import { ArrowDown, ArrowRight, CaretDown, SignOut } from "phosphor-react";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -18,38 +18,45 @@ function Settings() {
       >
         Trivia category:{" "}
       </label>
-      <select
-        onChange={e => dispatch(setCategory(e.target.value))}
-        value={category}
-        name="trivia_category"
-        className="h-20 bg-bgcolor2 px-4 rounded-lg w-full mb-4 font-medium"
-      >
-        <option value="any">Any Category</option>
-        <option value="9">General Knowledge</option>
-        <option value="10">Entertainment: Books</option>
-        <option value="11">Entertainment: Film</option>
-        <option value="12">Entertainment: Music</option>
-        <option value="13">Entertainment: Musicals & Theatres</option>
-        <option value="14">Entertainment: Television</option>
-        <option value="15">Entertainment: Video Games</option>
-        <option value="16">Entertainment: Board Games</option>
-        <option value="17">Science & Nature</option>
-        <option value="18">Science: Computers</option>
-        <option value="19">Science: Mathematics</option>
-        <option value="20">Mythology</option>
-        <option value="21">Sports</option>
-        <option value="22">Geography</option>
-        <option value="23">History</option>
-        <option value="24">Politics</option>
-        <option value="25">Art</option>
-        <option value="26">Celebrities</option>
-        <option value="27">Animals</option>
-        <option value="28">Vehicles</option>
-        <option value="29">Entertainment: Comics</option>
-        <option value="30">Science: Gadgets</option>
-        <option value="31">Entertainment: Japanese Anime & Manga</option>
-        <option value="32">Entertainment: Cartoon & Animations</option>
-      </select>
+      <div className="relative mb-4">
+        <select
+          onChange={e => dispatch(setCategory(e.target.value))}
+          value={category}
+          name="trivia_category"
+          className="h-20 bg-bgcolor2 px-5 appearance-none rounded-lg w-full cursor-pointer font-medium"
+        >
+          <option value="any">Any Category</option>
+          <option value="9">General Knowledge</option>
+          <option value="10">Entertainment: Books</option>
+          <option value="11">Entertainment: Film</option>
+          <option value="12">Entertainment: Music</option>
+          <option value="13">Entertainment: Musicals & Theatres</option>
+          <option value="14">Entertainment: Television</option>
+          <option value="15">Entertainment: Video Games</option>
+          <option value="16">Entertainment: Board Games</option>
+          <option value="17">Science & Nature</option>
+          <option value="18">Science: Computers</option>
+          <option value="19">Science: Mathematics</option>
+          <option value="20">Mythology</option>
+          <option value="21">Sports</option>
+          <option value="22">Geography</option>
+          <option value="23">History</option>
+          <option value="24">Politics</option>
+          <option value="25">Art</option>
+          <option value="26">Celebrities</option>
+          <option value="27">Animals</option>
+          <option value="28">Vehicles</option>
+          <option value="29">Entertainment: Comics</option>
+          <option value="30">Science: Gadgets</option>
+          <option value="31">Entertainment: Japanese Anime & Manga</option>
+          <option value="32">Entertainment: Cartoon & Animations</option>
+        </select>
+        <CaretDown
+          className="absolute text-txtcolor right-5 my-auto top-1/2 -translate-y-1/2 z-99"
+          size="24px"
+          weight="bold"
+        />
+      </div>
 
       <button
         className="h-20 w-full px-5 font-xl font-bold text-white rounded-lg
@@ -57,9 +64,9 @@ function Settings() {
         type="button"
         onClick={() => history.push("/game") || dispatch(restartGame())}
       >
-       <span>Play</span>
+        <span>Play</span>
 
-       <ArrowRight weight="bold" size="24px" />
+        <ArrowRight weight="bold" size="24px" />
       </button>
 
       <div className="h-[2px] rounded w-full bg-stone-200 my-4" />
