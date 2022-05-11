dbf1d826ae534e3d05850aa9ae505dbcd2a23a68
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 21:24:32 -0300

    feat: redirect user to settings when clicking profile icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import md5 from "crypto-js/md5";
+import { useHistory } from "react-router-dom";
 
 export function Header() {
   const name = useSelector(state => state.player.name);
   const gravatarEmail = useSelector(state => state.player.gravatarEmail);
   const highScore = useSelector(state => state.player.highScore);
   const hashedImageUrl = md5(gravatarEmail).toString();
+  const history = useHistory();
 
   return (
-    <header className="bg-bgcolor2 m-4 py-2 gap-2 px-4 h-14 rounded-xl flex items-center justify-between overflow-hidden">
+    <header className="bg-bgcolor2 m-4 py-2 gap-8 px-4 h-14 rounded-xl flex items-center justify-between overflow-hidden">
       <h1 className="text-xl text-center shrink-0">High score: {highScore}</h1>
       <div className="flex justify-end gap-4 grow items-center">
         <h1 className="text-xl shrink line-clamp-1 overflow-ellipsis">{name}</h1>
@@ -17,6 +19,7 @@ export function Header() {
           src={`https://www.gravatar.com/avatar/${hashedImageUrl}`}
           alt={name}
           className="rounded-full h-10 shrink-0"
+          onClick={() => history.push("/settings")}
         />
       </div>
     </header>
