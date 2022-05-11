9b6e4cf30f4b72a5449850be088ac55209454eb5
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-11 01:05:44 -0300

    chore: update util index

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,7 @@
-export { default as getToken } from './getToken';
-export { loadState, saveState } from './localStorage';
-export { default as shuffle } from './shuffle';
-export { default as parseHTML } from './parseHTML';
+export { default as getToken } from "./getToken";
+export { loadState, saveState } from "./localStorage";
+export { default as parseHTML } from "./parseHTML";
+export { default as renderWithHeader } from "./renderWithHeader";
+export { default as setupTheme } from "./setupTheme";
+export { default as shuffle } from "./shuffle";
+export { default as toggleTheme } from "./toggleTheme";
