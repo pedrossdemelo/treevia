97c4e042c61d11bd1bd7d5ef0cf629f49fac7423
Pedro Sousa <pedrossm2000@gmail.com>
2022-05-10 22:15:12 -0300

    chore: remove useless token

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
-import tokenReducer from './tokenReducer';
 
 const rootReducer = combineReducers({
   player: userReducer,
-  token: tokenReducer,
 });
 
 export default rootReducer;
